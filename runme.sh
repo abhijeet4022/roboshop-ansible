138c9acb53e265871fa80ef10f4ebf77a728abd6
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-25 10:57:08 +0530

    Terraform: ansible runme.sh

---
+++ runme.sh
@@ -0,0 +1,9 @@
+domain=dev.learntechnology.tech
+components=("frontend" "mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
+password=ansible_password
+
+for component in "${components[@]}"; do
+  inventory="${component}-${domain}"
+  ansible-playbook -i "$inventory" -e ansible_user=centos -e ansible_password="$password" -e component="$component" main.yml
+done
+
