31de950fb157312fca8ccaef6d0a71e4c5a3b15e
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-25 11:06:12 +0530

    Terraform: ansible runme.sh

--- runme.sh
+++ runme.sh
@@ -1,6 +1,10 @@
 domain=dev.learntechnology.tech
 components=("frontend" "mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
-password=ansible_password
+password="$ansible_password"
+if [ -z "${password}" ]; then
+  echo -e "\e[31mPlease: Mention the password for ansible_user.\e[0m"
+  exit 1
+fi
 
 for component in "${components[@]}"; do
   inventory="${component}-${domain},"
